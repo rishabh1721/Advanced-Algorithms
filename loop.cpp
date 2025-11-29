b04caf7ec6749982f30cb4cafed0cf617601c5f2
rishabh1721 <rishabhmcu001@gmail.com>
2025-11-29 05:49:34 +0530

    Initial commit

---
+++ loop.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main() {
+    
+
+//   for(int num = 0 ; num >= 0 ; num++){
+    
+//     cout << "number: " << num << " ";
+//   }
+
+
+    
+// 
+// int n ;
+// cout
+// << "Enter a positive integer: ";
+// cin >> n; 
+// int sum = 0;
+// for(int i = 1; i<=n; i++){
+//   sum+=i;
+  
+// }
+// cout<< "sum =" <<sum<< " ";
+
+// int count = 0;
+// while(count <10){
+//   cout << count << " ";
+//   count++;
+// }
+
+for(int i = 1 ; i <=4; i++){
+  for(int j = 1 ; j <=4; j++){
+    cout << " * "  ;
+  }
+  cout  << endl;
+}
+
+}
---
+++ switch.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // int day;
+  // cout << "enter day number (1-7): ";
+  // cin >> day;
+ 
+  // switch(day){
+  // //   case 1:
+  // //     cout<<"Monday";
+  // //     break;
+  // //   case 2:
+  // //     cout<<"Tuesday";
+  // //     break;
+  // //   case 3:
+  // //     cout<<"Wednesday";    
+  // //     break;
+  // //   case 4:
+  // //     cout<<"Thursday";       
+
+  // //     break;        
+  // //   case 5:
+  // //     cout<<"Friday";
+  // //     break;
+  // //   case 6:
+  // //     cout<<"Saturday";
+  // //     break;
+  // //   case 7:
+  // //     cout<<"Sunday";
+  // //     break;
+  // //   default:      
+  // //     cout<<"Invalid day";
+  // //     break;
+
+
+  // // }
+
+  // int a , b ;
+  // char  op;
+  // cout << "Enter first number: ";
+  // cin >> a;
+  // cout << "Enter second number: ";
+  // cin >> b;   
+  // cout << "Enter operator (+, -, *, /): ";
+  // cin >> op;
+  // // if(op == '+'){
+  //   cout <<  "a + b = "<<(a + b) << endl ;
+  // }else if(op == '-'){
+  //   cout <<  "a - b = "<<(a - b) << endl ;
+  // }
+  // else if(op == '*'){
+  //   cout <<  "a * b = " << (a * b) << endl ;
+  // }
+  // else if(op == '/'){
+  //   cout <<  "a / b = " << (a / b) << endl ;
+  // }else {
+  //   cout << "Invalid operator" << endl;
+  // }
+
+
+
+
+//  // swtitch 
+
+// switch(op){
+//   case'+':
+//   cout <<  "a + b = " << (a + b) << endl ;
+//   break;
+//   case'-':
+//   cout <<  "a - b = " << (a - b) << endl ;
+//   break;    
+//   case'*':
+//   cout <<  "a * b = " << (a * b) << endl ;
+//   break;
+//   case'/':
+//   if(b != 0) {
+//     cout <<  "a / b = " << (a / b) << endl ;
+//   } else {
+//     cout << "Division by zero is not allowed." << endl;   
+//   }
+//   break;
+//   default:
+//   cout << "Invalid operator" << endl;     
+
+//   break;
+// }
+//   return 0;
+// } 
+   
+
+//ques 1;
+
+// int num;
+// cout << "enter a number:" << endl;
+// cin >> num;
+// if(num > 0){
+//   cout << "The number is positive." << endl;
+// }else if(num < 0){
+//   cout << "The number is negative." << endl;
+
+// }else{
+//   cout << "The number is zero." << endl;
+// }
+
+
+// ques 2;
+
+//  int year ; 
+// cout << "Enter a year: ";
+// cin >> year;
+// if(year % 4 == 0 ){
+//   if(year % 400== 0){
+//     cout << year << " is a leap year." << endl;
+//   }else {
+//     cout << year << " is a leap year." << endl;
+//   }
+// }else{
+//   cout << year << " is not a leap year." << endl;
+// }
+
+
+// ques 3
+
+//  int a , b , c;
+//  cout << "Enter three numbers: "; 
+//  cin >> a >> b >> c;
+//  if()
+
+
+
+// }
+
+}
