ef9a916cf55eae6ca81ad971c007210378d08842
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-11 03:09:09 +0530

    sixth-pattern

---
+++ patterns-advance/sixth.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void print6(int n)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= n - i + 1; j++)
+    {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main()
+
+{
+  int n;
+  cout << "Enter number of rows: ";
+  cin >> n;
+  print6(n);
+  return 0;
+}
