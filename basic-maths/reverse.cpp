cfe49b2477851aee2cfcb9c1f408eebb4c9bfa3d
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-12 15:47:26 +0530

    reverse-complete

---
+++ basic-maths/reverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout << "enter digit" << endl;
+  cin >> n;
+  int rev = 0;
+  while (n > 0)
+  {
+    int digit = n % 10;
+    rev = rev * 10 + digit;
+    n = n / 10;
+  }
+  cout << "reversed number is " << rev << endl;
+}
