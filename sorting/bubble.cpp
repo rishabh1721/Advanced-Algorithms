8dd9da019baa31e4889f5cc27c77209f851364b4
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-13 19:29:08 +0530

    bubble-sort

---
+++ sorting/bubble.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+#include <iostream>
+
+class Solution
+{
+public:
+  void bubbleSort(vector<int> &arr)
+  {
+
+    // code here
+
+    int n = arr.size();
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+      for (int j = 0; j <= i - 1; j++)
+      {
+        if (arr[j] > arr[j + 1])
+        {
+          int temp = arr[j + 1];
+          arr[j + 1] = arr[j];
+          arr[j] = temp;
+        }
+      }
+    }
+  }
+};
