4a0d54dd6cbeaecbc1241dc827c6e0797d20d0cc
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-14 17:22:18 +0530

    largest-element-in-an-array

---
+++ arrays/LargestElement.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+#include <algorithm>
+
+class Solution
+{
+public:
+  int largest(vector<int> &arr)
+  {
+    // code here
+    int largest = arr[0];
+    for (int i = 0; i < arr.size(); i++)
+    {
+      if (arr[i] > largest)
+      {
+        largest = arr[i];
+      }
+    }
+    return largest;
+  }
+};
