cbc125fe86623db6ec991fafc9eab2bfd4254021
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-16 03:19:39 +0530

    move-optimal-approach

---
+++ arrays/moveOptimal.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  void pushZerosToEnd(vector<int> &arr)
+  {
+    // code here
+    int n = arr.size();
+    int j = -1;
+    for (int i = 0; i < n; i++)
+    {
+      if (arr[i] == 0)
+      {
+        j = i;
+        break;
+      }
+    }
+    if (j == -1)
+    {
+      return;
+    }
+    for (int i = j + 1; i < n; i++)
+    {
+      if (arr[i] != 0)
+      {
+        swap(arr[i], arr[j]);
+        j++;
+      }
+    }
+  }
+};
