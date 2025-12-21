5f25444d0a11ceec41872a9fa350b5246d7c9729
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-22 00:13:01 +0530

    matrix-0sto0s - brute

---
+++ arrays/matrix0s1s.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+class Solution
+{
+public:
+  void setMatrixZeroes(vector<vector<int>> &mat)
+  {
+    int n = mat.size();
+    int m = mat[0].size();
+    vector<int> row(n, 0);
+    vector<int> col(m, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        if (mat[i][j] == 0)
+        {
+          row[i] = 1;
+          col[j] = 1;
+        }
+      }
+    }
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        if (row[i] || col[j])
+        {
+          mat[i][j] = 0;
+        }
+      }
+    }
+  }
+};
