3299b4bf32707e28fac225582c09b18869ee3d5f
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-23 23:55:32 +0530

    xor-subarray

---
+++ arrays/xor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#include <map>
+class Solution
+{
+public:
+  long subarrayXor(vector<int> &arr, int k)
+  {
+    // code here
+    int xr = 0;
+    int cnt = 0;
+    map<int, int> mpp;
+    mpp[xr]++;
+    for (int i = 0; i < arr.size(); i++)
+    {
+      xr = xr ^ arr[i];
+      int x = xr ^ k;
+      cnt += mpp[x];
+      mpp[xr]++;
+    }
+    return cnt;
+  }
+};
