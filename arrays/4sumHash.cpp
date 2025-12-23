c4824f003259f70efdbbc728d3b083fdb52e8d47
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-23 22:23:02 +0530

    4sum hashing

---
+++ arrays/4sumHash.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+  vector<vector<int>> fourSum(vector<int> &arr, int target)
+  {
+    // code here
+    int n = arr.size();
+    set<vector<int>> st;
+    for (int i = 0; i < n; i++)
+    {
+
+      for (int j = i + 1; j < n; j++)
+      {
+        set<long long> hashset;
+
+        for (int k = j + 1; k < n; k++)
+        {
+
+          long long sum = arr[i] + arr[j];
+          sum += arr[k];
+          long long fourth = target - sum;
+          if (hashset.find(fourth) != hashset.end())
+          {
+            vector<int> temp = {arr[i], arr[j], arr[k], (int)fourth};
+            sort(temp.begin(), temp.end());
+            st.insert(temp);
+          }
+          hashset.insert(arr[k]);
+        }
+      }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+  }
+};
