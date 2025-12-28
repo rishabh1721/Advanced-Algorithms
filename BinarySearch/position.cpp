f8fae14d56de8b870b2fb87bdf1c25394062f406
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-28 23:21:42 +0530

    position-to-enter

---
+++ BinarySearch/position.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  int searchInsert(vector<int> &arr, int k)
+  {
+    int low = 0;
+    int high = arr.size() - 1;
+    int ans = arr.size();
+    while (low <= high)
+    {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] >= k)
+      {
+        ans = mid;
+        high = mid - 1;
+      }
+      else
+      {
+        low = mid + 1;
+      }
+    }
+    return ans;
+  }
+};
