133dbc990b4ebd83cbc139c5bbe90a37a5c48e3a
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-28 21:57:00 +0530

    bs-lower-bound

---
+++ BinarySearch/bsLowerB.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  int lowerBound(vector<int> &arr, int target)
+  {
+    // code here
+    int ans = arr.size();
+    int low = 0;
+    int high = arr.size() - 1;
+    while (low <= high)
+    {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] >= target)
+      {
+        ans = mid;
+        high = mid - 1;
+      }
+      else
+      {
+        low = mid + 1;
+      }
+    }
+    return ans;
+  }
+};
