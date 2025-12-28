2088443837d433ad2e5961ab96307b0502bb494c
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-28 23:35:47 +0530

    floor-in-array

---
+++ BinarySearch/floor.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  int findFloor(vector<int> &arr, int x)
+  {
+    // code here
+    int ans = -1;
+    int low = 0;
+    int high = arr.size() - 1;
+    while (low <= high)
+    {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] <= x)
+      {
+        ans = mid;
+        low = mid + 1;
+      }
+      else
+      {
+        high = mid - 1;
+      }
+    }
+    return ans;
+  }
+};
