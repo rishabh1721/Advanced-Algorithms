9a979e9c04055e6cff53bca828ab7ee97e60c2db
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-29 14:06:02 +0530

    search-in-rotated-array

---
+++ BinarySearch/bsRotateArray.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int search(vector<int> &arr, int target)
+  {
+    int low = 0;
+    int high = arr.size() - 1;
+    while (low <= high)
+    {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] == target)
+        return mid;
+
+      // left sorted
+
+      if (arr[low] <= arr[mid])
+      {
+        if (arr[low] <= target && target < arr[mid])
+        {
+          high = mid - 1;
+        }
+        else
+        {
+          low = mid + 1;
+        }
+      }
+      else
+      {
+        if (arr[mid] <= target && target <= arr[high])
+        {
+          low = mid + 1;
+        }
+        else
+        {
+          high = mid - 1;
+        }
+      }
+    }
+    return -1;
+  }
+};
