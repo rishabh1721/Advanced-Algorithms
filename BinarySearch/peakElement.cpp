9729061f414c7a96eb8af8f07bd1b5d047cfd323
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-29 17:32:54 +0530

    peak-element

---
+++ BinarySearch/peakElement.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  int peakElement(vector<int> &arr)
+  {
+    // code here
+    int low = 0;
+    int high = arr.size() - 1;
+    while (low < high)
+    {
+      int mid = low + (high - low) / 2;
+      if (arr[mid] < arr[mid + 1])
+      {
+        low = mid + 1;
+      }
+      else
+      {
+        high = mid;
+      }
+    }
+    return low;
+  }
+};
