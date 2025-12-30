60f4f5139637c045e54cc48e42b0460d0fcb0514
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-30 22:26:25 +0530

    search in a sorted matrix

---
+++ BinarySearch/search2dsorted.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  bool searchMatrix(vector<vector<int>> &mat, int x)
+  {
+    int n = mat.size();
+    int m = mat[0].size();
+    int low = 0;
+    int high = (n * m) - 1;
+    while (low <= high)
+    {
+      int mid = low + (high - low) / 2;
+      int row = mid / m;
+      int col = mid % m;
+
+      if (mat[row][col] == x)
+        return true;
+      else if (mat[row][col] <= x)
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+    return false;
+  }
+};
