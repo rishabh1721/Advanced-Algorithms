5287121055822f75ffb92a7f7aa9de97ff755492
rishabh1721 <rishabhmcu001@gmail.com>
2026-01-04 14:20:25 +0530

    zigzag-conversion

---
+++ recursion-advance/zigzag.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+  string convert(string s, int numRows)
+  {
+    if (numRows == 1 || numRows >= (int)s.size())
+      return s;
+
+    vector<string> rows(numRows);
+    int curRow = 0;
+    int dir = 1;
+
+    for (char ch : s)
+    {
+      rows[curRow].push_back(ch);
+      if (curRow == 0)
+        dir = 1;
+      else if (curRow == numRows - 1)
+        dir = -1;
+      curRow += dir;
+    }
+
+    string res;
+    for (auto &row : rows)
+      res += row;
+    return res;
+  }
+};
