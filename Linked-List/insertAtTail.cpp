4d11aaac315f9b3b00e36d3da75f820b1204e380
rishabh1721 <rishabhmcu001@gmail.com>
2026-01-06 12:17:01 +0530

    insert-at-tail

---
+++ Linked-List/insertAtTail.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+  int data;
+  Node *next;
+
+  Node(int data1, Node *next1) : data(data1), next(next1) {}
+};
+
+Node *convertArr2LL(vector<int> &arr)
+{
+  if (arr.size() == 0)
+    return nullptr;
+
+  Node *head = new Node(arr[0], nullptr);
+  Node *mover = head;
+
+  for (int i = 1; i < arr.size(); i++)
+  {
+    Node *temp = new Node(arr[i], nullptr);
+    mover->next = temp;
+    mover = temp;
+  }
+  return head;
+}
+
+Node *insertHead(Node *head, int val)
+{
+  Node *temp = new Node(val, head);
+  return temp;
+}
+
+Node *insertTail(Node *head, int val)
+{
+  if (head == NULL)
+  {
+    return new Node(val, nullptr);
+  }
+  Node *temp = head;
+  while (temp->next != NULL)
+  {
+    temp = temp->next;
+  }
+  Node *newNode = new Node(val, nullptr);
+  temp->next = newNode;
+  return head;
+}
+
+int main()
+{
+  vector<int> arr = {2, 5, 8, 7};
+
+  Node *head = convertArr2LL(arr);
+
+  // print linked list
+  Node *cur = head;
+  while (cur != nullptr)
+  {
+    cout << cur->data << " ";
+    cur = cur->next;
+  }
+  head = insertTail(head, 100);
+}
