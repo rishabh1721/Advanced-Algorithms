386e127cc39d3c47496f618298068502f06abf7f
rishabh1721 <rishabhmcu001@gmail.com>
2026-01-07 08:47:52 +0530

    delete-tail-of-doubly-linked-list

---
+++ Linked-List/FundamentalsDoublyLL/deleteAtTailDll.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node *back;
+
+  Node(int data1, Node *next1 = nullptr, Node *back1 = nullptr)
+      : data(data1), next(next1), back(back1) {}
+};
+
+Node *convertArrToDLL(vector<int> &arr)
+{
+  if (arr.empty())
+    return nullptr;
+
+  Node *head = new Node(arr[0]);
+  Node *prev = head;
+
+  for (int i = 1; i < arr.size(); i++)
+  {
+    Node *curr = new Node(arr[i]);
+    prev->next = curr;
+    curr->back = prev;
+    prev = curr;
+  }
+
+  return head;
+}
+
+void print(Node *head)
+{
+  while (head != nullptr)
+  {
+    cout << head->data << " ";
+    head = head->next;
+  }
+}
+
+Node *deleteTail(Node *head)
+{
+  if (head == nullptr || head->next == nullptr)
+    return nullptr;
+  Node *tail = head;
+  while (tail->next != nullptr)
+  {
+    tail = tail->next;
+  }
+  Node *prev = tail->back;
+  tail->back = nullptr;
+  prev->next = nullptr;
+  delete tail;
+  return head;
+}
+
+int main()
+{
+  vector<int> arr = {2, 3, 5, 8};
+  Node *head = convertArrToDLL(arr);
+  deleteTail(head);
+  print(head);
+  return 0;
+}
