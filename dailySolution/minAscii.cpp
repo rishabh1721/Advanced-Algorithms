676eb3ab484a85d7db5ba4e9775fec9c64119f26
rishabh1721 <rishabhmcu001@gmail.com>
2026-01-10 19:13:06 +0530

    minimum-ascii

---
+++ dailySolution/minAscii.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+  int minimumDeleteSum(string s1, string s2)
+  {
+    int n = s1.size(), m = s2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    // DP to compute max ASCII sum of common subsequence
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= m; j++)
+      {
+        if (s1[i - 1] == s2[j - 1])
+        {
+          dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
+        }
+        else
+        {
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+      }
+    }
+
+    int sum1 = 0, sum2 = 0;
+    for (char c : s1)
+      sum1 += c;
+    for (char c : s2)
+      sum2 += c;
+
+    return sum1 + sum2 - 2 * dp[n][m];
+  }
+};
