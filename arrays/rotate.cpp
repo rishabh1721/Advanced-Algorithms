62c750285ed5149aae9126816679a5725bea1a69
rishabh1721 <rishabhmcu001@gmail.com>
2026-01-15 16:32:10 +0530

    rotate

---
+++ arrays/rotate.cpp
@@ -0,0 +1,19 @@
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+
+public:
+  void rotateArr(vector<int> &arr, int d)
+  {
+    int n = arr.size();
+    d = d % n;
+
+    reverse(arr.begin(), arr.begin() + d);
+    reverse(arr.begin() + d, arr.end());
+    reverse(arr.begin(), arr.end());
+  }
+};
