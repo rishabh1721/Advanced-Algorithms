e7dd4203131140145d9343e78191480621ceda2e
rishabh1721 <rishabhmcu001@gmail.com>
2026-01-20 03:15:40 +0530

    optimal tortoise and hare

---
+++ Linked-List/MediumProblems/middleOptimal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+
+  Node(int x)
+  {
+    data = x;
+    next = NULL;
+  }
+};
+
+class Solution
+{
+public:
+  int getMiddle(Node *head)
+  {
+    // code here
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != nullptr && fast->next != nullptr)
+    {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    return slow->data;
+  }
+};
