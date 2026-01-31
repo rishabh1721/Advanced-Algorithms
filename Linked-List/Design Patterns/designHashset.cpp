bb1e5fa058132ddf7a44f6c34339331e3548de57
rishabh1721 <rishabhmcu001@gmail.com>
2026-02-01 03:02:23 +0530

    design hashset

---
+++ Linked-List/Design Patterns/designHashset.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+using namespace std;
+class MyHashSet
+{
+private:
+  vector<bool> set;
+
+public:
+  MyHashSet()
+  {
+    set.resize(1000001, false);
+  }
+
+  void add(int key)
+  {
+    set[key] = true;
+  }
+
+  void remove(int key)
+  {
+    set[key] = false;
+  }
+
+  bool contains(int key)
+  {
+    return set[key];
+  }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
