15f4b1bdaffba29a54446cc8085e0520b36eb999
rishabh1721 <rishabhmcu001@gmail.com>
2026-02-20 17:29:17 +0530

    bits-2

---
+++ bit manipulation/minFlipsBrian.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+  int minBitFlips(int start, int goal)
+  {
+    int x = start ^ goal;
+    int cnt = 0;
+    while (x)
+    {
+      x &= (x - 1);
+      cnt++;
+    }
+    return cnt;
+  }
+};
---
+++ bit manipulation/minbitsFlipBetter.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+  int minBitFlips(int start, int goal)
+  {
+    int x = start ^ goal;
+    int cnt = 0;
+    while (x)
+    {
+      cnt += x & 1;
+      x >>= 1;
+    }
+    return cnt;
+  }
+};
