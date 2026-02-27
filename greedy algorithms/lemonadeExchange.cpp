5a47ce125a2711355b0a48bb5804b7191953225b
rishabh1721 <rishabhmcu001@gmail.com>
2026-02-28 00:30:01 +0530

    lemonade exchange

---
+++ greedy algorithms/lemonadeExchange.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten =0;
+        for(int i=0; i<bills.size(); i++){
+            if(bills[i]== 5){
+                five++;
+            }else if(bills[i]==10){
+                if(five){
+                    five--;
+                    ten++;
+                }else{
+                    return false;
+                }
+            }
+            else{
+                if(five && ten){
+                    ten--;
+                    five--;
+                }else if(five >= 3){
+                    five = five -3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
