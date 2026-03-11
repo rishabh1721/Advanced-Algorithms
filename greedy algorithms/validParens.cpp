dc61668cc02d7e095b11f6c8b8ebe4994d197471
rishabh1721 <rishabhmcu001@gmail.com>
2026-03-11 14:43:45 +0530

    valid parenthesis

---
+++ greedy algorithms/validParens.cpp
@@ -0,0 +1,29 @@
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int minOpen =0;
+        int maxOpen =0;
+        for(char c: s){
+            if(c == '('){
+                minOpen++;
+                maxOpen++;
+            }
+            else if(c == ')'){
+                minOpen--;
+                maxOpen--;
+            }else if(c == '*'){
+                minOpen--;
+                maxOpen++;
+            }
+            if( maxOpen < 0) return false;
+            if(minOpen < 0){
+                minOpen = 0;
+            }
+        }
+        return (minOpen == 0);
+    }
+};
