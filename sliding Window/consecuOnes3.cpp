f1471910bd14def38f8b556c24128dbfac075d59
rishabh1721 <rishabhmcu001@gmail.com>
2026-03-12 15:29:23 +0530

    max consecutive ones 3

---
+++ sliding Window/consecuOnes3.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int l=0, r=0, zero=0,maxLen =0;
+        while(r < nums.size()){
+            if(nums[r] == 0) zero++;
+            if(zero > k){
+                if(nums[l]==0){
+                    zero--;
+                
+            }
+            l++;
+
+            }
+            if(zero <= k){
+                int len = r-l+1;
+                maxLen = max(len, maxLen);
+                
+            }
+            r++;
+            
+        }
+        return maxLen;
+
+    }
+};
