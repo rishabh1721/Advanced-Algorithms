6cda41f14185a1de775f8682d8cd01d7144e6625
rishabh1721 <rishabhmcu001@gmail.com>
2026-03-15 14:15:24 +0530

    nge 1

---
+++ Stack & Queue/nge1.cpp
@@ -0,0 +1,30 @@
+#include<stack>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int,int>mp;
+        for(int num : nums2){
+            while(!st.empty() && st.top() < num){
+                mp[st.top()] = num;
+                st.pop();
+            }
+            st.push(num);
+        }
+        while(!st.empty()){
+            mp[st.top()] = -1;
+            st.pop();
+        }
+        vector<int> ans;
+        for(int num : nums1){
+        ans.push_back(mp[num]);
+        }
+        return ans;
+
+    }
+    
+};
