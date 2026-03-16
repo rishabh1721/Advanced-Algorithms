842f325c42903aaf054900c83a01a2250cf07814
rishabh1721 <rishabhmcu001@gmail.com>
2026-03-16 15:10:48 +0530

    remove k

---
+++ Stack & Queue/removekDigits.cpp
@@ -0,0 +1,43 @@
+#include<string>
+#include<stack>
+#include<algorithm>
+
+
+using namespace std;
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        for(int i=0; i<num.size(); i++){
+            char digit = num[i];
+            while(!st.empty() && k > 0 && (st.top()-'0') > (num[i]-'0')){
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+        }
+        while(!st.empty() && k> 0){
+            st.pop();
+            k--;
+        }
+        if(st.empty()){
+             return "0";
+        }
+        string res = "";
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+         reverse(res.begin(), res.end());
+
+        int i = 0;
+        while(i < res.size() && res[i] == '0') i++;
+
+        res = res.substr(i);
+
+        if(res.empty()) return "0";
+
+        return res;
+    }
+};
