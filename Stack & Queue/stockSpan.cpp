4e81c197e2bc669cf77f635b7d142bb94da392be
rishabh1721 <rishabhmcu001@gmail.com>
2026-03-17 16:16:16 +0530

    stock span

---
+++ Stack & Queue/stockSpan.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> span(n);
+        stack<int> st; // store indices
+
+        for(int i = 0; i < n; i++){
+
+            while(!st.empty() && arr[st.top()] <= arr[i]){
+                st.pop();
+            }
+
+            if(st.empty())
+                span[i] = i + 1;
+            else
+                span[i] = i - st.top();
+
+            st.push(i);
+        }
+
+        return span;
+    }
+};
