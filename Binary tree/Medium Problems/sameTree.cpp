92138a59fec07dd9bc1cc1bfb04c9eecc49d878f
rishabh1721 <rishabhmcu001@gmail.com>
2026-04-15 10:02:11 +0530

    same tree

---
+++ Binary tree/Medium Problems/sameTree.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+  public:
+    bool isIdentical(Node* p, Node* q) {
+        // code here
+        if(p==nullptr || q==nullptr){
+            return (p==q);
+        }
+        return (p->data == q->data)
+        && isIdentical(p->left,q->left)
+        && isIdentical(p->right,q->right);
+    }
+};
