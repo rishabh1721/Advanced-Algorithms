37e0dae7346b64a609fe0d32b66ed0bdd6b1de48
rishabh1721 <rishabhmcu001@gmail.com>
2026-05-16 14:20:24 +0530

    right view of bt

---
+++ Binary tree/FAQS/rightView.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+
+class Solution {
+  public:
+    vector<int> rightView(Node *root) {
+        //  code here
+        vector<int> res;
+        recur(root,0,res);
+        return res;
+    }
+    void recur(Node* node, int level, vector<int>& res){
+        if(node==nullptr) return;
+        if(level == res.size()) res.push_back(node->data);
+        recur(node->right,level+1,res);
+        recur(node->left,level+1,res);
+
+    }
+};
